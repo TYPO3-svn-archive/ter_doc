8188f21dd9de33bbbe56020e4959ec42d3247239
flyguide <flyguide@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-04-22 21:57:30 +0000

    whitespace clean up
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ter_doc/trunk@46948 735d13b6-9817-0410-8766-e36946ffe9aa

--- cli/render-documents_cli.php
+++ cli/render-documents_cli.php
@@ -39,10 +39,14 @@ define('TYPO3_cliMode', TRUE);
 #define('PATH_thisScript',$_ENV['_']?$_ENV['_']:$_SERVER['_']);
 define('PATH_thisScript',$_SERVER['SCRIPT_FILENAME']);
 
-require(dirname(PATH_thisScript).'/conf.php');
-require(dirname(PATH_thisScript).'/'.$BACK_PATH.'init.php');
 
-require_once t3lib_extMgm::extPath('ter_doc').'class.tx_terdoc_renderdocuments.php'; 
+
+require(dirname(PATH_thisScript).'/conf.php');
+#$BACK_PATH = '/Users/ingo/Projects/typo3.org/t3o_2011/www/html/typo3/';
+#require(dirname(PATH_thisScript).'/'.$BACK_PATH.'init.php');
+require('/Users/ingo/Projects/typo3.org/t3o_2011/www/html/typo3/init.php');
+
+require_once t3lib_extMgm::extPath('ter_doc').'class.tx_terdoc_renderdocuments.php';
 
 $renderDocsObj = tx_terdoc_renderdocuments::getInstance();
 $renderDocsObj->renderCache();
