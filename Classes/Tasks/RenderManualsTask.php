6d440f1b1db1ddccedb257eca5f31f202c784cf6
flyguide <flyguide@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-09-17 21:39:36 +0000

    fixed indentation
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ter_doc/trunk@52091 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/Tasks/RenderManualsTask.php
+++ Classes/Tasks/RenderManualsTask.php
@@ -1,48 +1,48 @@
 <?php
-	/***************************************************************
-	 *  Copyright notice
-	 *
-	 *  (c) 2011 Kai Vogel (kai.vogel@speedprogs.de)
-	 *  All rights reserved
-	 *
-	 *  This script is part of the TYPO3 project. The TYPO3 project is
-	 *  free software; you can redistribute it and/or modify
-	 *  it under the terms of the GNU General Public License as published by
-	 *  the Free Software Foundation; either version 2 of the License, or
-	 *  (at your option) any later version.
-	 *
-	 *  The GNU General Public License can be found at
-	 *  http://www.gnu.org/copyleft/gpl.html.
-	 *
-	 *  This script is distributed in the hope that it will be useful,
-	 *  but WITHOUT ANY WARRANTY; without even the implied warranty of
-	 *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	 *  GNU General Public License for more details.
-	 *
-	 *  This copyright notice MUST APPEAR in all copies of the script!
-	 ***************************************************************/
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2011 Kai Vogel (kai.vogel@speedprogs.de)
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 
-	require_once t3lib_extMgm::extPath('ter_doc') . 'class.tx_terdoc_renderdocuments.php';
+require_once t3lib_extMgm::extPath('ter_doc') . 'class.tx_terdoc_renderdocuments.php';
+
+/**
+ * Render manuals of new extensions in TER
+ *
+ * @author Kai Vogel <kai.vogel@speedprogs.de>
+ * @package TYPO3
+ * @subpackage tx_terdoc
+ */
+class Tx_TerDoc_Tasks_RenderManualsTask extends tx_scheduler_Task {
 
 	/**
-	 * Render manuals of new extensions in TER
+	 * Public method, usually called by scheduler
 	 *
-	 * @author Kai Vogel <kai.vogel@speedprogs.de>
-	 * @package TYPO3
-	 * @subpackage tx_terdoc
+	 * @return boolean TRUE on success
 	 */
-	class Tx_TerDoc_Tasks_RenderManualsTask extends tx_scheduler_Task {
-
-		/**
-		 * Public method, usually called by scheduler
-		 *
-		 * @return boolean TRUE on success
-		 */
-		public function execute() {
-			$renderDocsObj = tx_terdoc_renderdocuments::getInstance();
-			$renderDocsObj->renderCache();
-			return TRUE;
-		}
-
+	public function execute() {
+		$renderDocsObj = tx_terdoc_renderdocuments::getInstance();
+		$renderDocsObj->renderCache();
+		return TRUE;
 	}
+
+}
 ?>
