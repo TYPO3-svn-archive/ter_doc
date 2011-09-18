ae770274d48c84422f271af667201db54f99e838
flyguide <flyguide@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-09-18 10:31:19 +0000

    fixed the @return annotation of getInstance()
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ter_doc/trunk@52118 735d13b6-9817-0410-8766-e36946ffe9aa

--- class.tx_terdoc_api.php
+++ class.tx_terdoc_api.php
@@ -42,7 +42,7 @@ class tx_terdoc_api {
 	/**
 	 * This constructor is private because you may only instantiate this class by calling
 	 * the function getInstance() which returns a unique instance of this class (Singleton).
-	 * 
+	 *
 	 * @return		void
 	 * @access		private
 	 */
@@ -52,21 +52,21 @@ class tx_terdoc_api {
 	/**
 	 * Returns a unique instance of this class. Call this function instead of creating a new
 	 * instance manually!
-	 * 
-	 * @return		object		Unique instance of tx_terdoc_renderdocuments
+	 *
+	 * @return		tx_terdoc_api		Unique instance of tx_terdoc_renderdocuments
 	 * @access		public
 	 */
 	public function getInstance() {
 		if (self::$instance === FALSE) {
 			self::$instance = new tx_terdoc_api;
-			self::$instance->init();	
+			self::$instance->init();
 		}
-		return self::$instance;	
-	} 
+		return self::$instance;
+	}
 
 	/**
 	 * Initializes this class
-	 * 
+	 *
 	 * @return	void
 	 * @access	protected
 	 */
@@ -75,7 +75,7 @@ class tx_terdoc_api {
 			// Fetch static configuration from Extension Manager:
 		$staticConfArr = unserialize ($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['ter_doc']);
 		if (is_array ($staticConfArr)) {
-			
+
 			$this->repositoryDir = $staticConfArr['repositoryDir'];
 			if (substr ($this->repositoryDir, -1, 1) != '/') $this->repositoryDir .= '/';
 
@@ -88,13 +88,13 @@ class tx_terdoc_api {
 	/**
 	 * Returns one level of a rendered Table Of Content of the given extension version. If no manual exists,
 	 * a message will be returned instead.
-	 *  
+	 *
 	 * Note: The parameter $withLinks only works if the extension ter_doc_html is installed.
-	 * 
+	 *
 	 * @param		string		$extensionKey: Extension key of the manual
 	 * @param		string		$version: Version string of the manual
 	 * @param		boolean		$withLinks: If set, the TOC entries are linked with a page which actually displays the chapters and sections
-	 * @param		boolean		$smartLevels: Usually the first level (chapters) will be rendered. If the first level only conntains one entry and $smartLevels is set, the second level will be rendered instead. (Compatibility option for older OOo manuals where first level is always the extension title)  
+	 * @param		boolean		$smartLevels: Usually the first level (chapters) will be rendered. If the first level only conntains one entry and $smartLevels is set, the second level will be rendered instead. (Compatibility option for older OOo manuals where first level is always the extension title)
 	 * @return		string		HTML code of the rendered TOC
 	 * @access		public
 	 */
@@ -103,13 +103,13 @@ class tx_terdoc_api {
 
 		$documentDir = $this->getDocumentDirOfExtensionVersion ($extensionKey, $version);
 		$tocArr = unserialize (@file_get_contents ($documentDir.'toc.dat'));
-		if (!is_array ($tocArr)) return '<strong style="color:red;">'.htmlspecialchars($TSFE->sL('LLL:EXT:ter_doc/locallang.xml:api_error_documentationnotavailable')).'</strong>';		
+		if (!is_array ($tocArr)) return '<strong style="color:red;">'.htmlspecialchars($TSFE->sL('LLL:EXT:ter_doc/locallang.xml:api_error_documentationnotavailable')).'</strong>';
 
-		$levelToRenderArr = ($smartLevels && count ($tocArr) == 1) ? $tocArr[1]['sections'] : $tocArr;				
+		$levelToRenderArr = ($smartLevels && count ($tocArr) == 1) ? $tocArr[1]['sections'] : $tocArr;
 
 		if (is_array ($levelToRenderArr)) {
-			$pageId = $this->getViewPageIdForExtensionVersion($extensionKey, $version); 
-			
+			$pageId = $this->getViewPageIdForExtensionVersion($extensionKey, $version);
+
 			foreach ($levelToRenderArr as $itemsArr) {
 				if ($withLinks && t3lib_extMgm::isLoaded ('ter_doc_html')) {
 					$parametersArr = array(
@@ -121,25 +121,25 @@ class tx_terdoc_api {
 					$typoLinkConf = array (
 						'parameter' => $pageId,
 						'additionalParams' => t3lib_div::implodeArrayForUrl('', $parametersArr),
-						'useCacheHash' => 1						
+						'useCacheHash' => 1
 					);
 					$item = $TSFE->cObj->typoLink($this->csConvHSC($itemsArr['title']), $typoLinkConf);
 				} else {
-					$item = $this->csConvHSC($itemsArr['title']);	
+					$item = $this->csConvHSC($itemsArr['title']);
 				}
-				
+
 				$output .= $item.'<br />';
 			}
 		} else {
-			$output = '<strong style="color:red;">'.htmlspecialchars($TSFE->sL('LLL:EXT:ter_doc/locallang.xml:api_error_tocempty')).'</strong>';	
+			$output = '<strong style="color:red;">'.htmlspecialchars($TSFE->sL('LLL:EXT:ter_doc/locallang.xml:api_error_tocempty')).'</strong>';
 		}
-		
+
 		return $output;
 	}
 
 	/**
-	 * Returns a link to the documentation (table of contents) of the given extension version 
-	 * 
+	 * Returns a link to the documentation (table of contents) of the given extension version
+	 *
 	 * @param		string		$extensionKey: Extension key of the manual
 	 * @param		string		$version: Version string of the manual
 	 * @return		string		HTML code of the link
@@ -160,7 +160,7 @@ class tx_terdoc_api {
 				$message = '<span style="color:red;">'.htmlspecialchars($TSFE->sL('LLL:EXT:ter_doc/locallang.xml:api_error_documentationnotyetrendered')).'</span>';
 			}
 			return $message;
-		}		
+		}
 
 		$renderDocumentsObj = tx_terdoc_renderdocuments::getInstance();
 		$outputFormatsArr = $renderDocumentsObj->getOutputFormats();
@@ -171,29 +171,29 @@ class tx_terdoc_api {
 			return $message;
 		}
 
-		$pageId = $this->getViewPageIdForExtensionVersion($extensionKey, $version); 
+		$pageId = $this->getViewPageIdForExtensionVersion($extensionKey, $version);
 		$parametersArr = array(
 			'tx_terdoc_pi1[extensionkey]' => $extensionKey,
 			'tx_terdoc_pi1[version]' => 'current',
 			//'tx_terdoc_pi1[format]' => 'ter_doc_html_onlinehtml',
 			//'tx_terdoc_pi1[html_readonline_chapter]' => '',
 		);
-		
+
 		$label = $this->csConvHSC($TSFE->sL($outputFormatsArr['ter_doc_html_onlinehtml']['label']));
 		$typoLinkConf = array (
 			'parameter' => $pageId,
 			'additionalParams' => t3lib_div::implodeArrayForUrl('', $parametersArr),
-			'useCacheHash' => 1						
+			'useCacheHash' => 1
 		);
 
 		$link = $TSFE->cObj->typoLink($this->csConvHSC($label), $typoLinkConf);
-		
+
 		return $link;
 	}
-	
+
 	/**
 	 * Returns a link to a page listing all documentation render problems of the specified extension
-	 * 
+	 *
 	 * @param		string		$extensionKey: Extension key of the manual
 	 * @param		string		$version: Version string of the manual
 	 * @param 		string		$label: If set, this label is used for the link
@@ -212,25 +212,25 @@ class tx_terdoc_api {
 			return '';
 		}
 
-		$pageId = $this->getViewPageIdForExtensionVersion($extensionKey, $version); 
+		$pageId = $this->getViewPageIdForExtensionVersion($extensionKey, $version);
 		$parametersArr = array(
 			'tx_terdoc_pi1[extensionkey]' => $extensionKey,
 			'tx_terdoc_pi1[version]' => $version,
 			'tx_terdoc_pi1[format]' => 'ter_doc_renderproblems',
 		);
-		
+
 		$label = (!is_null($label) ? $label : $this->csConvHSC($TSFE->sL($outputFormatsArr['ter_doc_renderproblems']['label'])));
 		$typoLinkConf = array (
 			'parameter' => $pageId,
 			'additionalParams' => t3lib_div::implodeArrayForUrl('', $parametersArr),
-			'useCacheHash' => 1						
+			'useCacheHash' => 1
 		);
 
 		$link = $TSFE->cObj->typoLink($this->csConvHSC($label), $typoLinkConf);
-		
+
 		return $link;
 	}
-	
+
 
 
 
@@ -239,11 +239,11 @@ class tx_terdoc_api {
 	 * Returns the page ID of the page where the category of the specified extension
 	 * version can be read online. Of course that requires that a ter_doc frontend
 	 * plugin is installed on that page.
-	 * 
-	 * The PIDs for each category must be set in the ter_doc_categories record.  
-	 * 
+	 *
+	 * The PIDs for each category must be set in the ter_doc_categories record.
+	 *
 	 * If ter_doc_html is not installed, FALSE will be returned.
-	 * 
+	 *
 	 * @param	string		$extensionKey: The extension key
 	 * @param	string		$version: The version string
 	 * @return	mixed		Page ID (integer) or FALSE
@@ -251,7 +251,7 @@ class tx_terdoc_api {
 	 */
 	public function getViewPageIdForExtensionVersion ($extensionKey, $version) {
 		global $TYPO3_DB;
-		
+
 		if (!t3lib_extMgm::isLoaded ('ter_doc_html')) return FALSE;
 
 		$res = $TYPO3_DB->exec_SELECTquery (
@@ -260,7 +260,7 @@ class tx_terdoc_api {
 			'extensionkey="'.$TYPO3_DB->quoteStr($extensionKey, 'tx_terdoc_manualscategories').'" AND pid=' . (int) $this->storagePid
 		);
 		if (!$res) return FALSE;
-		
+
 		if ($TYPO3_DB->sql_num_rows ($res) == 0) {
 			$res = $TYPO3_DB->exec_SELECTquery (
 				'viewpid',
@@ -269,11 +269,11 @@ class tx_terdoc_api {
 			);
 			if (!$res) return FALSE;
 			$categoryRow = $TYPO3_DB->sql_fetch_assoc ($res);
-		
+
 			return $categoryRow['viewpid'];
 		} else {
 			$assignmentRow = $TYPO3_DB->sql_fetch_assoc ($res);
-			
+
 			$res = $TYPO3_DB->exec_SELECTquery (
 				'viewpid',
 				'tx_terdoc_categories',
@@ -281,25 +281,25 @@ class tx_terdoc_api {
 			);
 			if (!$res) return FALSE;
 			$categoryRow = $TYPO3_DB->sql_fetch_assoc ($res);
-		
+
 			return $categoryRow['viewpid'];
-		}		
+		}
 	}
 
 	/**
 	 * Returns a unique id (integer) for the combinition of the specified extension key
 	 * and version string. This UID can be used as a "reg1" parameter for the page
 	 * caching so later the cache can be cleared for a certain manual.
-	 * 
+	 *
 	 * If such a uid does not exist for the combinition of extension key and version string,
 	 * a database record will be created.
-	 * 
+	 *
 	 * @param	string		$extensionKey: The extension key
 	 * @param	string		$version: The version string
-	 * @return	mixed		The terdoc internal cache uid or FALSE if an error ocurred 
+	 * @return	mixed		The terdoc internal cache uid or FALSE if an error ocurred
 	 * @access	public
 	 */
-	
+
 	public function createAndGetCacheUidForExtensionVersion ($extensionKey, $version) {
 		global $TYPO3_DB;
 
@@ -308,11 +308,11 @@ class tx_terdoc_api {
 			'tx_terdoc_manualspagecache',
 			'extensionkey="'.$TYPO3_DB->quoteStr($extensionKey,'tx_terdoc_manualspagecache').'" AND version="'.$TYPO3_DB->quoteStr($version,'tx_terdoc_manualspagecache').'" AND pid=' . (int) $this->storagePid
 		);
-		if (!$res) return FALSE; 
+		if (!$res) return FALSE;
 
 		if ($TYPO3_DB->sql_num_rows ($res) > 0) {
 			$row = $TYPO3_DB->sql_fetch_assoc ($res);
-			return is_array ($row) ? $row['uid'] : FALSE;			
+			return is_array ($row) ? $row['uid'] : FALSE;
 		} else {
 			$fields = array(
 				'pid' => (int) $this->storagePid,
@@ -321,19 +321,19 @@ class tx_terdoc_api {
 			);
 			$res = $TYPO3_DB->exec_INSERTquery ('tx_terdoc_manualspagecache', $fields);
 			if (!$res) return FALSE;
-			
+
 			return $TYPO3_DB->sql_insert_id ($res);
 		}
-		
+
 	}
 
 	/**
 	 * Returns the full path of the document directory for the specified
-	 * extension version. If the path does not exist yet, it will be created - 
-	 * given that the typo3temp/tx_terdoc/documentscache/ dir exists.  
-	 * 
+	 * extension version. If the path does not exist yet, it will be created -
+	 * given that the typo3temp/tx_terdoc/documentscache/ dir exists.
+	 *
 	 * In the document directory all rendered documents are stored.
-	 * 
+	 *
 	 * @param	string		$extensionKey: The extension key
 	 * @param	string		$version: The version string
 	 * @return	string		Full path to the document directory for the specified extension version
@@ -346,15 +346,15 @@ class tx_terdoc_api {
 
  		list ($majorVersion, $minorVersion, $devVersion) = t3lib_div::intExplode ('.', $version);
 		$fullPath = $baseDir.$firstLetter.'/'.$secondLetter.'/'.strtolower($extensionKey).'-'.$majorVersion.'.'.$minorVersion.'.'.$devVersion;
-						
-		return $fullPath.'/';		
+
+		return $fullPath.'/';
 	}
-	
+
 	/**
 	 * Processes the given string with htmlspecialchars and converts the result
 	 * from utf-8 to the charset of the current frontend
-	 * page 
-	 * 
+	 * page
+	 *
 	 * @param	string	$string: The utf-8 string to convert
 	 * @return	string	The converted string
 	 * @access	public
