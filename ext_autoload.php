36746f22b7744c75090330e8fb7b3a07b1868bdb
tolleiv <tolleiv@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-04-18 14:20:07 +0000

    Update ext_autoload.php
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ter_doc/branches/renderQueue@60843 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_autoload.php
+++ ext_autoload.php
@@ -1,10 +1,22 @@
 <?php
-$extensionClassesPath = t3lib_extMgm::extPath('ter_doc', 'Classes/');
+
+$extensionPath = t3lib_extMgm::extPath('ter_doc');
 return array(
-	'tx_terdoc_utility_cli' => $extensionClassesPath . 'Utility/Cli.php',
-	'tx_terdoc_controller_clicontroller' => $extensionClassesPath . 'Controller/CliController.php',
-	'tx_terdoc_domain_repository_extensionrepository' => $extensionClassesPath . 'Domain/Repository/ExtensionRepository.php',
-	'tx_terdoc_validator_environment' => $extensionClassesPath . 'Validator/Environment.php',
-	'tx_terdoc_tasks_rendermanualstask' => $extensionClassesPath . 'Tasks/RenderManualsTask.php',
+	'tx_terdoc_api' => $extensionPath . 'class.tx_terdoc_api.php',
+	'tx_terdoc_documentformat' => $extensionPath . 'class.tx_terdoc_documentformat.php',
+	'tx_terdoc_documentformat_index' => $extensionPath . 'class.tx_terdoc_documentformat.php',
+	'tx_terdoc_documentformat_display' => $extensionPath . 'class.tx_terdoc_documentformat.php',
+	'tx_terdoc_documentformat_download' => $extensionPath . 'class.tx_terdoc_documentformat.php',
+	'tx_terdoc_renderdocuments' => $extensionPath . 'class.tx_terdoc_renderdocuments.php',
+	'tx_terdoc_cli_renderer' => $extensionPath . 'Classes/Cli/Renderer.php',
+	'tx_terdoc_controller_clicontroller' => $extensionPath . 'Classes/Controller/CliController.php',
+	'tx_terdoc_domain_model_queueitem' => $extensionPath . 'Classes/Domain/Model/QueueItem.php',
+	'tx_terdoc_domain_repository_extensionrepository' => $extensionPath . 'Classes/Domain/Repository/ExtensionRepository.php',
+	'tx_terdoc_domain_repository_queueitemrepository' => $extensionPath . 'Classes/Domain/Repository/QueueItemRepository.php',
+	'tx_terdoc_tasks_rendermanualstask' => $extensionPath . 'Classes/Tasks/RenderManualsTask.php',
+	'tx_terdoc_utility_cli' => $extensionPath . 'Classes/Utility/Cli.php',
+	'tx_terdoc_validator_environment' => $extensionPath . 'Classes/Validator/Environment.php',
+	'tx_terdoc_module1' => $extensionPath . 'mod1/index.php',
+	'tx_terdoc_pi1' => $extensionPath . 'pi1/class.tx_terdoc_pi1.php',
 );
 ?>
